b9f42ba02a232bdb32610f33e678efd527ac206b
Miguel Santa Cruz <pg50661@alunos.uminho.pt>
2022-10-20 12:42:19 +0100

    Cliente servidor atualizados

--- Cliente.py
+++ Cliente.py
@@ -1,13 +1,18 @@
-# echo-client.py
-
 import socket
+
+from pexpect import EOF
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 8080  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-
-print(f"Received {data!r}")
+    while True:
+        try:
+            string = input()
+        except EOFError:
+             break
+        s.sendall(bytes(string,"utf-8"))
+        data = s.recv(1024)
+        print("Server response: " + str(data,"utf-8"))
+s.close()
--- Servidor.py
+++ Servidor.py
@@ -1,16 +1,23 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"
 PORT = 8080
 
+
+def clientHandler(connection, clientAddress):
+    with connection:
+        print(f"Connected by {clientAdress}")
+        while connection :
+            data = connection.recv(1024)
+            if not data:
+                break
+            connection.sendall(data)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    while True:
+        connection, clientAdress = s.accept()
+        thread = threading.Thread(target=clientHandler, args=(connection,clientAdress,))
+        thread.start()
